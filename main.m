f9467a2afde24b944e0af59057c7f03eda3c2d3e
Tetsuro Kurita <tkurita@mac.com>
2006-04-21 13:31:37 +0000

    [miclient] 最新のコードで書き直し。mi のアプリケーションパスの検索は、クリエータタイプだけで行うようにした。
    git-svn-id: file:///Users/tkurita/Dev/svnrepos/miclient/trunk@1061 a4934b6f-310c-0410-bed4-ac220e363503

---
+++ main.m
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#import "miClientLib/miClient.h"
+
+#define useLog 0 //Yes:1, No:0
+
+
+void usage() {
+	//fprintf(stderr, "Usage: miclient [-b] [line] file \n");
+	printf("Usage: miclient [-b] [line] file \n");
+	exit(-1);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+int main (int argc, char * const argv[]) {
+	
+	/* get arguments */
+	Boolean bFlag = false;
+	while(getopt(argc, argv, "b") != -1 ){
+		switch(optopt){
+			case 'b': bFlag = true ; break;
+			case '?':
+			default	:
+				usage(); break;
+		}
+		optarg	=	NULL;
+	}
+	
+	long parIndex = 0;
+	char * filePath;
+	
+	if (optind < argc) {
+		if (argc - optind > 2) {
+			// too many arguments
+			usage();
+		}
+		
+		filePath = argv[--argc];
+		//printf("filePath : %s\n",filePath);
+		if (optind < argc) {
+			parIndex = atol(argv[--argc]); 
+			//printf("parIndex : %i\n",parIndex);
+		}
+	}
+	else {
+		usage();
+	}
+		
+	/* check file path */
+	FSRef fileRef;	
+	OSErr err = FSPathMakeRef ((UInt8 *) filePath, &fileRef, NULL);
+	if (err != noErr) {
+		//fprintf(stderr,"%s is not found.\n",filePath);
+		printf("Error in miclient : %s is not found.\n",filePath);
+		exit(-1);
+	}
+	
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	id miclient = [[[miClient alloc] init] autorelease];
+	BOOL isSuccess = [miclient jumpToFile:&fileRef paragraph:[NSNumber numberWithLong:parIndex]];
+	
+	[pool release];
+	if (isSuccess)
+		return 0;
+	else
+		return 1;
+}
